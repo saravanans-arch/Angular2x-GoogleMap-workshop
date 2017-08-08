082e9f72543a2420ce276ae3ba0112bb583e21b7
Saravanan <saravanans.arch@gmail.com>
2017-08-08 13:01:38 +0530

    Map commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Angular2x Google Map Project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
